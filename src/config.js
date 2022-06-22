df87b759613229191cb4345602e94e846f74eb95
taha.almokahel@gmail.com <taha.almokahel@gmail.com>
2022-06-22 14:34:15 +0300

    connect front with back end cloud server

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ config.fxcm_api_port = 443;
 config.fxcm_api_proto = 'https'; // http or https
 config.fxcm_full_api_host = config.fxcm_api_proto + '://' + config.fxcm_api_host + ':' + config.fxcm_api_port;
 
-config.analyzer_api_host = 'localhost';
-config.analyzer_api_port = 5000;
-config.analyzer_api_proto = 'http'; // http or https
+config.analyzer_api_host = 'analyzer-back-end-iuautt5bsa-uc.a.run.app'; // 'localhost';
+// config.analyzer_api_port = 5000;
+config.analyzer_api_proto = 'https'; // http or https
 
 config.analyzer_full_api_host = config.analyzer_api_proto + '://' + config.analyzer_api_host + ':' + config.analyzer_api_port;
 
